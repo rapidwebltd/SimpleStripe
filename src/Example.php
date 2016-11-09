10a8a6b961fd026ed0fbe433a0d75744a61d8126
Jordan Hall <jordan.hall@rapidweb.biz>
2016-11-09 12:01:29 +0000

    Improvements to example file

--- src/Example.php
+++ src/Example.php
@@ -1,28 +1,36 @@
 <?php
-
 require_once '../vendor/autoload.php';
 
-use \RapidWeb\SimpleStripe\Factories\SimpleStripeFactory;
+// Setup SimpleStripe using Stripe API keys and currency 
+$simpleStripe = \RapidWeb\SimpleStripe\Factories\SimpleStripeFactory::create('PUBLISHABLE_KEY', 'SECRET_KEY', 'GBP'); // GBP = Great British Pounds
 
-$simpleStripe = SimpleStripeFactory::create('PUBLISHABLE_KEY', 'SECRET_KEY', 'GBP'); // GBP = Great British Pounds
-
+// If payment form has been submitted
 if (isset($_POST['stripeToken'])) {
 
+    // Get the amount to charge (in the currency's lowest denomination)
     $amount = 500; // Five hundred pence = Five pounds (5 GBP)
 
+    // Charge the customer
     $charge = $simpleStripe->charge($amount, $_POST['stripeToken']);
 
     if ($charge->succeeded) {
+        
+        // If charge succeeded, display success messsage
         echo "Success! <a href=\"Example.php\">Make another payment.</a>";
+
+    } elseif ($charge->problemType=='Card') {
+
+        // If there was a problem with the card, display details of the problem
+        echo $charge->problem;
+
     } else {
-        if ($charge->problemType=='Card') {
-            echo $charge->problem;
-        } else {
-            echo "Sorry, there was a problem processing your payment. <a href=\"Example.php\">Please try again.</a>";
-        }
+
+        // Else, display a generic failure message
+        echo "Sorry, there was a problem processing your payment. <a href=\"Example.php\">Please try again.</a>";
     }
 
     die;
 }
 
+// Display a simple payment form
 echo $simpleStripe->paymentForm();
